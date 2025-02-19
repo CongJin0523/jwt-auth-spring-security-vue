6fea2832c1eb3b0b2b42a0f3c9fad53d7443de16
Cong J <jincong0523@outlook.com>
2025-02-19 14:48:35 +0100

    init

---
+++ boot-backend/src/main/java/com/cong/BootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootBackendApplication.class, args);
+  }
+
+}
