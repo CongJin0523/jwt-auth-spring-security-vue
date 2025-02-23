82c059eefdbf685164211ef31935e54b9bef7413
Cong J <jincong0523@outlook.com>
2025-02-23 12:33:07 +0100

    feature: home page and login function

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "@/router"
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8080'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
